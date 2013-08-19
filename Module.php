6cdead00fa4eb3652e01d54fe2754315d02b2272
mxc-commons <frank.hein@maxence.de>
2013-08-19 19:34:20 +0200

    TEST
    Added these lines to test if modules.zendframework.com recognizes this
    module. Will be removed after registration.

--- Module.php
+++ Module.php
@@ -4,4 +4,9 @@
  * It allows usage of this module even without composer.
  * The original Module.php is in 'src/MxcLayoutScheme' in order to respect PSR-0
  */
+
+// modules.zendframework.com does not recognize this module without the next two lines  
+namespace MxcLayoutScheme;
+class Module {}
+
 require_once __DIR__ . '/src/MxcLayoutScheme/Module.php';
